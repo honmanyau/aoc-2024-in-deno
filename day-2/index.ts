1c5f4a4917bcc2b93b15cc27b0f4e7370a8041e9
Honman Yau <22585758+honmanyau@users.noreply.github.com>
2024-12-02 17:18:12 +1100

    Implement `isSafeReport()`.

--- day-2/index.ts
+++ day-2/index.ts
@@ -14,5 +14,23 @@ export async function readPuzzleInput(path: string): Promise<number[][]> {
 }
 
 export function isSafeReport(report: number[]): boolean {
-    return false;
+    if (report.length < 2) {
+        throw new Error("Report must contain more than one element!");
+    }
+
+    if (report[0] === report[1]) return false;
+
+    const ascending = report[0] < report[1];
+
+    for (let i = 0; i < report.length - 2; i++) {
+        // Report is unsafe as soon a there is a direction change.
+        if (report[i] < report[i + 1] !== ascending) return false;
+
+        const difference = Math.abs(report[i + 1] - report[i]);
+
+        if (difference < 1) return false;
+        if (difference > 3) return false;
+    }
+
+    return true;
 }
