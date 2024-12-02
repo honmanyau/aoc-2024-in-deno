34c9042a6a02da136ad58d07b6143a9c8467879a
Honman Yau <22585758+honmanyau@users.noreply.github.com>
2024-12-02 16:58:23 +1100

    Day 2 setup.

---
+++ day-2/index.test.ts
@@ -0,0 +1,9 @@
+import { describe, it } from "@std/testing/bdd";
+
+import {} from "./index.ts";
+
+describe("Day 2", () => {
+    describe("readPuzzleInput()", () => {
+        it("succeeds", async () => {});
+    });
+});
---
+++ day-2/index.ts
@@ -0,0 +1,7 @@
+export async function solveDay2Part1(): Promise<void> {
+    return;
+}
+
+export async function solveDay2Part2(): Promise<void> {
+    return;
+}
