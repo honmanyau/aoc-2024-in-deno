7988591a8bddf7051129e5a288bcb734e932fb2b
Honman Yau <22585758+honmanyau@users.noreply.github.com>
2024-12-02 17:05:25 +1100

    Add test for `readPuzzleInput()`.

--- day-2/index.test.ts
+++ day-2/index.test.ts
@@ -1,9 +1,22 @@
 import { describe, it } from "@std/testing/bdd";
 
-import {} from "./index.ts";
+import { expect } from "@std/expect/expect";
+import { readPuzzleInput } from "./index.ts";
 
 describe("Day 2", () => {
     describe("readPuzzleInput()", () => {
-        it("succeeds", async () => {});
+        it("returns and array of arrays of integers of equal length", async () => {
+            const path = `${Deno.cwd()}/day-2/sample-input.txt`;
+            const input = await readPuzzleInput(path);
+
+            expect(input).toEqual([
+                [7, 6, 4, 2, 1],
+                [1, 2, 7, 8, 9],
+                [9, 7, 6, 2, 1],
+                [1, 3, 2, 4, 5],
+                [8, 6, 4, 4, 1],
+                [1, 3, 6, 7, 9],
+            ]);
+        });
     });
 });
--- day-2/index.ts
+++ day-2/index.ts
@@ -5,3 +5,7 @@ export async function solveDay2Part1(): Promise<void> {
 export async function solveDay2Part2(): Promise<void> {
     return;
 }
+
+export async function readPuzzleInput(path: string): Promise<number[][]> {
+    return [];
+}
