4faa88e2006b4f7e5d63ecf7b62904c52f1b849a
Honman Yau <22585758+honmanyau@users.noreply.github.com>
2024-12-03 16:52:34 +1100

    Add test for `readPuzzleInput()`.

--- day-3/index.test.ts
+++ day-3/index.test.ts
@@ -9,7 +9,9 @@ describe("Day 3", () => {
             const path = `${Deno.cwd()}/day-3/sample-input.txt`;
             const input = await readPuzzleInput(path);
 
-            expect(false);
+            expect(input).toBe(
+                `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
+            );
         });
     });
 });
