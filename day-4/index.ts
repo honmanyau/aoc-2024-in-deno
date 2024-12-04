1aca0d36848db5602b9060dba4a9ff7ce6d7785e
Honman Yau <22585758+honmanyau@users.noreply.github.com>
2024-12-04 16:21:27 +1100

    Implement `readPuzzleInput()`.

--- day-4/index.ts
+++ day-4/index.ts
@@ -6,8 +6,8 @@ export async function solveDay4Part2(): Promise<number> {
     return -1;
 }
 
-export async function readPuzzleInput(path: string): Promise<string> {
+export async function readPuzzleInput(path: string): Promise<string[]> {
     const content = await Deno.readTextFile(path);
 
-    return "";
+    return content.trim().split("\n");
 }
