634fc7eeee9455610fc09f5537ffe9eadccf9877
Honman Yau <22585758+honmanyau@users.noreply.github.com>
2024-12-13 16:06:22 +1100

    Add test for `readPuzzleInput()`.

--- day-13/index.test.ts
+++ day-13/index.test.ts
@@ -1,5 +1,6 @@
 import { describe, it } from "@std/testing/bdd";
 
+import { expect } from "@std/expect/expect";
 import { readPuzzleInput } from "./index.ts";
 
 describe("Day 13", () => {
@@ -7,6 +8,29 @@ describe("Day 13", () => {
         it("returns the puzzle input correctly for `sample-input`", async () => {
             const path = `${Deno.cwd()}/day-13/sample-input.txt`;
             const input = await readPuzzleInput(path);
+
+            expect(input).toBe([
+                [
+                    [94, 34],
+                    [22, 67],
+                    [8400, 5400],
+                ],
+                [
+                    [26, 66],
+                    [67, 21],
+                    [12748, 12176],
+                ],
+                [
+                    [17, 86],
+                    [84, 37],
+                    [7870, 6450],
+                ],
+                [
+                    [69, 23],
+                    [27, 71],
+                    [18641, 10279],
+                ],
+            ]);
         });
     });
 
