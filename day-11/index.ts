95db27dc093a48bdbda33dffcb8019c1ad9a1320
Honman Yau <22585758+honmanyau@users.noreply.github.com>
2024-12-13 23:33:11 +1100

    Refactor `blink()` to split out the flow that determines the next stones.

--- day-11/index.ts
+++ day-11/index.ts
@@ -24,20 +24,24 @@ export function blink(stones: Input): Input {
     const newStones: Input = [];
 
     for (const stone of stones) {
-        if (stone === "0") {
-            newStones.push("1");
-        } else if (stone.length % 2 === 0) {
-            const halfSize = stone.length / 2;
-            const leftStone = stone.slice(0, halfSize);
-            const rightStone = String(Number(stone.slice(halfSize)));
-
-            newStones.push(leftStone, rightStone);
-        } else {
-            newStones.push(String(Number(stone) * 2024));
-        }
+        newStones.push(...atomicBlink(stone));
     }
 
     return newStones;
+}
+
+export function atomicBlink(stone: string): string[] {
+    if (stone === "0") {
+        return ["1"];
+    } else if (stone.length % 2 === 0) {
+        const halfSize = stone.length / 2;
+        const leftStone = stone.slice(0, halfSize);
+        const rightStone = String(Number(stone.slice(halfSize)));
+
+        return [leftStone, rightStone];
+    }
+
+    return [String(Number(stone) * 2024)];
 }
 
 export function solvePart1(input: Input): number {
@@ -51,11 +55,5 @@ export function solvePart1(input: Input): number {
 }
 
 export function solvePart2(input: Input): number {
-    let result = input;
-
-    for (let i = 0; i < 75; i++) {
-        result = blink(result);
-    }
-
-    return result.length;
+    return -1;
 }
