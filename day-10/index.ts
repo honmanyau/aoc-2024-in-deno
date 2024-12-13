cdbd03f8809ed6a8f9a47cdbc0b6b61e2b9bcdf3
Honman Yau <22585758+honmanyau@users.noreply.github.com>
2024-12-13 20:49:13 +1100

    Day 10 part 2 done! ☀️ 🎉

--- day-10/index.ts
+++ day-10/index.ts
@@ -19,7 +19,7 @@ export async function solveDay10Part2(): Promise<number> {
     const path = `${Deno.cwd()}/day-10/input.txt`;
     const input = await readPuzzleInput(path);
 
-    return -1;
+    return solvePart2(input);
 }
 
 export async function readPuzzleInput(path: string): Promise<Input> {
