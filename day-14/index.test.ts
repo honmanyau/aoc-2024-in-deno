43b6075c7db294c8c4f68ee419dd1374fe1f4c07
Honman Yau <22585758+honmanyau@users.noreply.github.com>
2024-12-14 16:24:33 +1100

    Implement `readPuzzleInput()`.

--- day-14/index.test.ts
+++ day-14/index.test.ts
@@ -62,36 +62,6 @@ describe("Day 14", () => {
         });
     });
 
-    describe("readPuzzleInput()", () => {
-        it("returns the puzzle input correctly for `sample-input`", async () => {
-            const path = `${Deno.cwd()}/day-14/sample-input.txt`;
-            const input = await readPuzzleInput(path);
-
-            expect(input).toEqual([
-                [
-                    [94, 34],
-                    [22, 67],
-                    [8400, 5400],
-                ],
-                [
-                    [26, 66],
-                    [67, 21],
-                    [12748, 12176],
-                ],
-                [
-                    [17, 86],
-                    [84, 37],
-                    [7870, 6450],
-                ],
-                [
-                    [69, 23],
-                    [27, 71],
-                    [18641, 10279],
-                ],
-            ]);
-        });
-    });
-
     describe("solvePart1()", () => {
         it("returns", async () => {
             const path = `${Deno.cwd()}/day-14/sample-input.txt`;
--- day-14/index.ts
+++ day-14/index.ts
@@ -17,7 +17,19 @@ export async function solveDay14Part2(): Promise<number> {
 }
 
 export async function readPuzzleInput(path: string): Promise<Input> {
-    return [];
+    const content = await Deno.readTextFile(path);
+    const lines = content.trim().split("\n");
+
+    return lines.map((line) => {
+        const match = line.match(/p=(.+) v=(.+)/);
+
+        if (!match) throw new Error(`Invalid line parsing logic!`);
+
+        const position = match[1].split(",").map(Number).reverse() as Position;
+        const velocity = match[2].split(",").map(Number).reverse() as Vector;
+
+        return [position, velocity];
+    });
 }
 
 export function solvePart1(input: Input): number {
