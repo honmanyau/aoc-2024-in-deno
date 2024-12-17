282b51eb54e3a3a283865a0b000fd0a5accb0930
Honman Yau <22585758+honmanyau@users.noreply.github.com>
2024-12-17 16:30:05 +1100

    Implement `readPuzzleInput()`.

--- day-17/index.ts
+++ day-17/index.ts
@@ -1,4 +1,11 @@
 export type Input = string[];
+export type Registers = {
+    A: number;
+    B: number;
+    C: number;
+};
+
+export type Program = [string, number][];
 
 export async function solveDay17Part1(): Promise<number> {
     const path = `${Deno.cwd()}/day-17/input.txt`;
@@ -14,11 +21,29 @@ export async function solveDay17Part2(): Promise<number> {
     return -1;
 }
 
-export async function readPuzzleInput(path: string): Promise<Input> {
+export async function readPuzzleInput(
+    path: string
+): Promise<[Registers, Program]> {
     const content = await Deno.readTextFile(path);
-    const lines = content.split("\n");
+    const [registersText, programText] = content.split("\n\n");
+    const registers: Registers = {
+        A: Number(registersText.match(/Register A: (\d+)/)![1]),
+        B: Number(registersText.match(/Register B: (\d+)/)![1]),
+        C: Number(registersText.match(/Register C: (\d+)/)![1]),
+    };
+    const program = programText
+        .replace(/^Program: /, "")
+        .trim()
+        .split(",")
+        .reduce((acc, val, i, arr) => {
+            if (i % 2 === 0) {
+                acc.push([val, Number(arr[i + 1])]);
+            }
 
-    return lines;
+            return acc;
+        }, [] as Program);
+
+    return [registers, program];
 }
 
 export function solvePart1(input: Input): number {
